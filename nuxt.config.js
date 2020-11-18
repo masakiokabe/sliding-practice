b9508e036ea8d7bdf3afa58a1f8644f4230d9a16
masakiokabe <29qp2p237564@gmail.com>
2020-11-18 10:10:45 +0900

    Before deploy to firebase console

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~plugins/bootstrap.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -40,5 +41,6 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  
   }
 }
---
+++ pages/example.vue
@@ -0,0 +1,45 @@
+<template>
+  <hooper>
+    <slide>
+      slide 11111
+    </slide>
+    <slide>
+      slide 22222
+    </slide>
+    <slide>
+      slide 33333
+    </slide>
+    <slide>
+      slide 44444
+    </slide>
+    <slide>
+      slide 55555
+    </slide>
+    <slide>
+      slide 6666
+    </slide>
+    ...
+    <hooper-pagination slot="hooper-addons"></hooper-pagination>
+    <hooper-pagination slot="hooper-addons"></hooper-pagination>
+    <hooper-pagination slot="hooper-addons"></hooper-pagination>
+  </hooper>
+</template>
+
+<script>
+import 'hooper/dist/hooper.css';
+import {
+  Hooper,
+  Slide,
+  Pagination as HooperPagination
+  } from 'hooper';
+
+export default {
+  layout: 'none',  
+  name: 'App',
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination
+  }
+};
+</script>
